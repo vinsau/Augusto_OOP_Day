ccc74c28e7bf857b7d6718f2752ca1eeb03670d9
Vincent Augusto <vince.au23@gmail.com>
2024-02-13 22:24:23 +0800

    JAVA_OOP_CLASS_ACTIVITIES_DAY

---
+++ Java_OOP/src/java_oop/Class_Activity_1.java
@@ -0,0 +1,40 @@
+
+package java_oop;
+
+import java.util.Scanner;
+
+public class Class_Activity_1
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Input your Full Name(Last name, first name): ");
+        String fullName = input.nextLine();
+        
+        System.out.print("Input your Age: ");
+        int age = input.nextInt();
+        input.nextLine();
+        
+        System.out.print("Input your Gender: ");
+        String gender = input.nextLine();
+        
+        System.out.print("Input your Address: ");
+        String address = input.nextLine();
+        
+        System.out.print("Input your Civil Status: ");
+        String civilStatus = input.nextLine();
+        
+        System.out.print("Input the number of sibling(s) you have: ");
+        int numberOfSiblings = input.nextInt();
+        
+        input.close();
+        
+        System.out.println("\n\nName: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Address: " + address);
+        System.out.println("Civil Status: " + civilStatus);
+        System.out.println("Number of siblings: " + numberOfSiblings);
+    }
+}
